2618c6dddcb83036334d095bbfcccb5628d93807
vault-42 <vault-42@users.noreply.github.com>
2017-08-09 23:42:30 -0400

    fix: n-gram bug
    In my_recognizer bi/trigram recognizers the sentence index was not being tracked properly. Fix imporves the bi/tri-gram recognizer performance.
    
    Rolling the benefits into the asl_recognizer notebook in a future commit

--- my_recognizer.py
+++ my_recognizer.py
@@ -85,36 +85,36 @@ def recognize_unigram(models: dict, test_set: SinglesData, lm_scaling_factor: in
         print("Problem reading the language model from the ARPA file")
         raise
 
-
     # implement the recognizer
 
     # print("Total Length: {}".format(test_set.num_items))
-    for word_id in range(test_set.num_items):
-        log_ls = {}  # dict of log liklihoods of a word
-        best_score = float("-inf")  # best log_l thus far
-        best_guess = None  # best guess for what the word from the test set could be
-        x, lengths = test_set.get_item_Xlengths(word_id)
+    for video_num in test_set.sentences_index:
+        for word_id in test_set.sentences_index[video_num]:
+            log_ls = {}  # dict of log liklihoods of a word
+            best_score = float("-inf")  # best log_l thus far
+            best_guess = None  # best guess for what the word from the test set could be
+            x, lengths = test_set.get_item_Xlengths(word_id)
 
-        for word, model in models.items():
+            for word, model in models.items():
 
-            try:
-                # Assumes a HMM model
-                log_ls[word] = model.score(x, lengths)
-            except:
-                # Unable to process word with this model
-                log_ls[word] = float("-inf")
-            else:
-                # Remove a trailing digit from word if it has one before passing to language model
-                word_key = ''.join(word[:-1] if word[-1].isdigit() else word)
-                log_ls[word] = log_ls[word] + lm_scaling_factor * lm.log_p(word_key)
+                try:
+                    # Assumes a HMM model
+                    log_ls[word] = model.score(x, lengths)
+                except:
+                    # Unable to process word with this model
+                    log_ls[word] = float("-inf")
+                else:
+                    # Remove a trailing digit from word if it has one before passing to language model
+                    word_key = ''.join(word[:-1] if word[-1].isdigit() else word)
+                    log_ls[word] = log_ls[word] + lm_scaling_factor * lm.log_p(word_key)
 
-            if log_ls[word] > best_score:
-                best_score = log_ls[word]
-                best_guess = word
-                # print("New Best Guess for {}: {}".format(word_id, best_guess))
+                if log_ls[word] > best_score:
+                    best_score = log_ls[word]
+                    best_guess = word
+                    # print("New Best Guess for {}: {}".format(word_id, best_guess))
 
-        probabilities.append(log_ls)
-        guesses.append(best_guess)
+            probabilities.append(log_ls)
+            guesses.append(best_guess)
 
     return probabilities, guesses
 
@@ -151,41 +151,50 @@ def recognize_bigram(models: dict, test_set: SinglesData, lm_scaling_factor: int
 
     # implement the recognizer
     prev_sentence = 0  # previous word_id's sentence index
-    for word_id in range(test_set.num_items):
-        log_ls = {}  # dict of log liklihoods of a word
-        best_score = float("-inf")  # best log_l thus far
-        best_guess = None  # best guess for what the word from the test set could be
-        x, lengths = test_set.get_item_Xlengths(word_id)
-        curr_sentence = test_set.sentences_index
+    for video_num in test_set.sentences_index:
+        for word_id in test_set.sentences_index[video_num]:
+            log_ls = {}  # dict of log liklihoods of a word
+            best_score = float("-inf")  # best log_l thus far
+            best_guess = None  # best guess for what the word from the test set could be
+            x, lengths = test_set.get_item_Xlengths(word_id)
+            curr_sentence = video_num
 
-        for word, model in models.items():
+            for word, model in models.items():
 
-            try:
-                # Assumes a HMM model
-                log_ls[word] = model.score(x, lengths)
-            except:
-                # Unable to process word with this model
-                log_ls[word] = float("-inf")
-            else:
-                # Remove a trailing digit from word if it has one before passing to language model
-                if not guesses or curr_sentence != prev_sentence:  # First guess in a sentence
-                    prev_word = '<s>'
+                try:
+                    # Assumes a HMM model
+                    log_ls[word] = model.score(x, lengths)
+                except:
+                    # Unable to process word with this model
+                    log_ls[word] = float("-inf")
                 else:
-                    prev_word = guesses[-1]
-                    prev_word = ''.join(prev_word[:-1] if prev_word[-1].isdigit() else prev_word)
-                word_key = ''.join(word[:-1] if word[-1].isdigit() else word)
-                word_key = prev_word + ' ' + word_key  # Combine previous word and current word
-                log_ls[word] = log_ls[word] + lm_scaling_factor * lm.log_p(word_key)
+                    # Remove a trailing digit from word if it has one before passing to language model
+                    if not guesses or curr_sentence != prev_sentence:  # First guess in a sentence
+                        prev_word = '<s>'
+                    else:
+                        prev_word = guesses[-1]
+                        prev_word = ''.join(prev_word[:-1] if prev_word[-1].isdigit() else prev_word)
+                    word_key = ''.join(word[:-1] if word[-1].isdigit() else word)
+                    word_key = prev_word + ' ' + word_key  # Combine previous word and current word
+                    lang_model_probability = lm.log_p(word_key)
+                    '''
+                    print("----------------------------------")
+                    print("Current Sentence: {}".format(curr_sentence))
+                    print("Word Key: {}".format(word_key))
+                    print("HMM probability: {}".format(log_ls[word]))
+                    print("LM probability: {}".format(lang_model_probability*lm_scaling_factor))
+                    '''
+                    log_ls[word] = log_ls[word] + lm_scaling_factor * lang_model_probability
 
-            if log_ls[word] > best_score:
-                best_score = log_ls[word]
-                best_guess = word
-                # print("New Best Guess for {}: {}".format(word_id, best_guess))
+                if log_ls[word] > best_score:
+                    best_score = log_ls[word]
+                    best_guess = word
+                    # print("New Best Guess for {}: {}".format(word_id, best_guess))
 
-        prev_sentence = curr_sentence
+            prev_sentence = curr_sentence
 
-        probabilities.append(log_ls)
-        guesses.append(best_guess)
+            probabilities.append(log_ls)
+            guesses.append(best_guess)
 
     return probabilities, guesses
 
@@ -223,47 +232,48 @@ def recognize_trigram(models: dict, test_set: SinglesData, lm_scaling_factor: in
     # implement the recognizer
     prev_sentence = -1  # previous word_id's sentence index
     prev_prev_sentence = -2  # two words back, the word_id's sentence index
-    for word_id in range(test_set.num_items):
-        log_ls = {}  # dict of log liklihoods of a word
-        best_score = float("-inf")  # best log_l thus far
-        best_guess = None  # best guess for what the word from the test set could be
-        x, lengths = test_set.get_item_Xlengths(word_id)
-        curr_sentence = test_set.sentences_index
+    for video_num in test_set.sentences_index:
+        for word_id in test_set.sentences_index[video_num]:
+            log_ls = {}  # dict of log liklihoods of a word
+            best_score = float("-inf")  # best log_l thus far
+            best_guess = None  # best guess for what the word from the test set could be
+            x, lengths = test_set.get_item_Xlengths(word_id)
+            curr_sentence = video_num
 
-        for word, model in models.items():
+            for word, model in models.items():
 
-            try:
-                # Assumes a HMM model
-                log_ls[word] = model.score(x, lengths)
-            except:
-                # Unable to process word with this model
-                log_ls[word] = float("-inf")
-            else:
-                # Remove a trailing digit from word if it has one before passing to language model
-                if not guesses or curr_sentence != prev_sentence:  # First guess in a sentence
-                    prev_word = '<s>' + ' ' + '<s>'
-                elif prev_sentence != prev_prev_sentence: # Second word in a sentence
-                    prev_word = guesses[-1]
-                    prev_word = '<s>' + ' ' + ''.join(prev_word[:-1] if prev_word[-1].isdigit() else prev_word)
+                try:
+                    # Assumes a HMM model
+                    log_ls[word] = model.score(x, lengths)
+                except:
+                    # Unable to process word with this model
+                    log_ls[word] = float("-inf")
                 else:
-                    prev_word = guesses[-1]
-                    temp = guesses[-2]
-                    prev_word = ''.join(prev_word[:-1] if prev_word[-1].isdigit() else prev_word)
-                    temp = ''.join(temp[:-1] if temp[-1].isdigit() else temp)
-                    prev_word = temp + ' ' + prev_word
-                word_key = ''.join(word[:-1] if word[-1].isdigit() else word)
-                word_key = prev_word + ' ' + word_key  # Combine previous word and current word
-                log_ls[word] = log_ls[word] + lm_scaling_factor * lm.log_p(word_key)
+                    # Remove a trailing digit from word if it has one before passing to language model
+                    if not guesses or curr_sentence != prev_sentence:  # First guess in a sentence
+                        prev_word = '<s>' + ' ' + '<s>'
+                    elif prev_sentence != prev_prev_sentence:  # Second word in a sentence
+                        prev_word = guesses[-1]
+                        prev_word = '<s>' + ' ' + ''.join(prev_word[:-1] if prev_word[-1].isdigit() else prev_word)
+                    else:
+                        prev_word = guesses[-1]
+                        temp = guesses[-2]
+                        prev_word = ''.join(prev_word[:-1] if prev_word[-1].isdigit() else prev_word)
+                        temp = ''.join(temp[:-1] if temp[-1].isdigit() else temp)
+                        prev_word = temp + ' ' + prev_word
+                    word_key = ''.join(word[:-1] if word[-1].isdigit() else word)
+                    word_key = prev_word + ' ' + word_key  # Combine previous word and current word
+                    log_ls[word] = log_ls[word] + lm_scaling_factor * lm.log_p(word_key)
 
-            if log_ls[word] > best_score:
-                best_score = log_ls[word]
-                best_guess = word
-                # print("New Best Guess for {}: {}".format(word_id, best_guess))
+                if log_ls[word] > best_score:
+                    best_score = log_ls[word]
+                    best_guess = word
+                    # print("New Best Guess for {}: {}".format(word_id, best_guess))
 
-        prev_prev_sentence = prev_sentence
-        prev_sentence = curr_sentence
+            prev_prev_sentence = prev_sentence
+            prev_sentence = curr_sentence
 
-        probabilities.append(log_ls)
-        guesses.append(best_guess)
+            probabilities.append(log_ls)
+            guesses.append(best_guess)
 
     return probabilities, guesses
